20e4ef1305b3052b101e92d2ae7b1a906875d920
Alexandru <alexandruandrei659.aa@gmail.com>
2024-10-31 14:11:20 +0100

    .

--- app.py
+++ app.py
@@ -22,5 +22,10 @@ def hw3():
 def hw4():
     return render_template('hw4.html')
 
+@app.route('/hw5')
+def hw5():
+    return render_template('hw5.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000, debug=True)
